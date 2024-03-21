8a652f2b39bafa744f3c499d79c0a147e4ef8583
Robo Ropuch <magdudinf@gmail.com>
2024-03-21 21:13:42 +0100

    Add functions explanation

--- src/hello.py
+++ src/hello.py
@@ -8,23 +8,11 @@ def say_hi(name):
     """
     Return a welcoming statement.
 
-    :param name: required name of welocmed person.
-    :type name: str.
-    :return: message with welocome statement.
-    :rtype: str.
+    :param name: required name of a person to be welcomed
+    :type name: str
+    :return: message with welocome statement
+    :rtype: str
 
     """
     return f'Hi {name}'
 
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-
-    """
-    return ["eggs", "bacon", "spam"]
