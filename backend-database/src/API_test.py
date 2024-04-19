6ab03f6bb5fbb4650b44e71b9c90ecd013307c36
Franciszek <franekmalewski@gmail.com>
2024-04-19 16:24:29 +0200

    Changed API_test to use newly implemented functionalities, removed data_base_test since it was unnecessary

--- backend-database/src/API_test.py
+++ backend-database/src/API_test.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from api_classes import ApiAdapter, ApiFacade
+from database_classes import DatabaseFacade, DatabaseObserver, DatabaseAdapter
+
 #fa0f3d27-bfbe-40b4-8e77-1b279b68f627 API key
 #lewo góra: 52.221459, 21.009006
 #lewo dół: 52.216708, 21.009006
@@ -8,30 +11,27 @@ import json
 # 52.221459 >= x >= 52.216673
 # 21.016504 >= y >= 21.009006
 
-#t1 = requests.post('https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e927d-4ad3-9500-4ab9e55deb59&type=1&line=523&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627')
-#print(t1.json())
+api_adp = ApiAdapter()
+dat_adp = DatabaseAdapter()
+api_face = ApiFacade('fa0f3d27-bfbe-40b4-8e77-1b279b68f627', api_adp)
+dat_face = DatabaseFacade('tablica.db')
+dat_obs = DatabaseObserver(dat_face)
 
-response = requests.get("https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId=1001&busstopNr=01&line=102&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627").json()
-print(response)
-with open('e9.json', 'w') as f:
-    json.dump(response, f)
-#print(response.keys())
-#print(len(response))
-r1 = response['result'] #results of the API call
- # theres 8226
-#print(r1)
-print(len(r1))
-#r2 = r1[0] #a single result
-#print(r2)
-quit()
-#print(len(r2))
-#print(r2.keys()) 
-#r3 = r2['values'] # entering into the values of a single result
-#print(len(r3))
-#print(r3)
+stops = dat_adp.parse_database_stop_ids(dat_face.get_active_stops_id())
+print('got stop ids')
+timetables = api_face.get_timetables_for_stops(stops)
+print('got timetable from api')
+dat_face.set_timetables(timetables)
+print(dat_obs.prepare_database_hash())
+stops = dat_adp.parse_database_stop_ids(dat_face.get_active_stops_id())
+print('got stop ids')
+timetables = api_face.get_timetables_for_stops(stops)
+print('got timetable from api')
+dat_face.set_timetables(timetables)
+print(dat_obs.prepare_database_hash())
 
 politechnika_przystanki_temp = []
-for f in r1:
+#for f in r1:
    # if(f['values'][2]['value'] == 'Torfowa'):
         #print(f['values'])
         #id = f['values'][0]['value']
@@ -42,35 +42,7 @@ for f in r1:
          #   l = i['values'][0]['value']
          #   r2 = requests.get(f'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e60518c9f3238&busstopId={id}&busstopNr={nr}&line={l}&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627')
         #    print(r2.json())
-    if((52.221459 >= float(f['values'][4]['value']) >= 52.216673) and (21.016504 >= float(f['values'][5]['value']) >= 21.009006)):
-        politechnika_przystanki_temp.append(f['values'])
-        print(f['values'][4]['value']+", "+f['values'][5]['value'])
+    #if((52.222 >= float(f['values'][4]['value']) >= 52.215) and (21.017 >= float(f['values'][5]['value']) >= 21.000)):
+     #   politechnika_przystanki_temp.append(f['values'])
+        #print(f['values'][4]['value']+", "+f['values'][5]['value'])
 
-print(len(politechnika_przystanki_temp))
-    
-politechnika_przystanki = []
-for przystan in politechnika_przystanki_temp:
-    id = przystan[0]['value']
-    nr = przystan[1]['value']
-    nazwa = przystan[2]['value']
-    response = requests.get(f'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=88cd555f-6f31-43ca-9de4-66c479ad5942&busstopId={id}&busstopNr={nr}&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627')
-    print(f'nazwa: {nazwa}, id: {id}, słupek: {nr}')
-    print(response.json())
-    linie = []
-    for r in response.json()['result']:
-        linie.append(r['values'][0]['value'])
-    politechnika_przystanki.append([nazwa, id, nr, linie])
-
-print(politechnika_przystanki)
-
-for p in politechnika_przystanki:
-    nazwa = p[0]
-    id = p[1]
-    nr = p[2]
-    linie = p[3]
-    for l in linie:
-        response2 = requests.get(f'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId={id}&busstopNr={nr}&line={l}&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627')
-        print(response2.json())
-
-#https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e60518c9f3238&busStopId={id}&busStopNr={nr}&line={l}&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627
-#https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId=2346&busstopNr=01&line=198&apikey=fa0f3d27-bfbe-40b4-8e77-1b279b68f627
