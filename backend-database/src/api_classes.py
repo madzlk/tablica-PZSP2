b857478ed0f34bc945313dfef7d3f79bb4264962
Franciszek <franekmalewski@gmail.com>
2024-04-21 08:31:07 +0200

    Adding files that i've worked on back into the repo, i sure hope i won't have to do that again.

---
+++ backend-database/src/api_classes.py
@@ -0,0 +1,171 @@
+import requests
+from datetime import datetime, timedelta
+
+class CouldNotConnect(Exception):
+    pass
+
+class IncorrectApiCall(Exception):
+    pass
+
+# Functions that are deterministic (can be tested):
+# Data Observer:
+# - prepare hash
+# - check for change
+# 
+# Api Facade:
+# Similar to database facade, none, since we would be testing our mocked functions and not the actual connection to the api
+#
+# Api Adapter:
+# - parse api stops
+# - parse api lines
+# - parse api timetables
+# - fix time
+
+# Status of tests: All written, all passing
+
+#===============================================DATABASE OBSERVER==============================================
+# This Class monitors for changes in the data using a hash,
+# and when a new state of the data is detected, it sends the signal to all of it's subscribers via
+# the "Update data" method, all subscribers need to have this method implemented.
+class DataObserver:
+    def __init__(self):
+        self.subscribers = []
+        self.data_hash = 0
+
+# Creates a hash from the data to use for comparison 
+# ignores unnecessary fields such as ids or timestamps (which get updated every time the data is updated)
+    def prepare_hash(self, data):
+        data_hash = hash(str(data))
+        return data_hash
+    
+    def add_subscriber(self, sub):
+        self.subscribers.append(sub)
+
+# If the state has changed, change internal state to new state and return True (to signal change)
+    def check_for_change(self, data):
+        new_hash = self.prepare_hash(data)
+        if new_hash != self.data_hash:
+            self.data_hash = new_hash
+            return True
+        return False
+    
+# Check for changes and if changes have occured, send new data state to subscribers,
+# otherwise sends the signal to update the time (since the data hasn't changed, but it has been checked.)
+    def update(self, data):
+        if self.check_for_change(data):
+            for sub in self.subscribers:
+                sub.update_data(data)
+        else:
+            for sub in self.subscribers:
+                sub.update_time()
+
+#============================================API FACADE======================================================
+# Provides a convenient interface for working with the API
+class ApiFacade:
+    def __init__(self, api_key, api_adapter):
+        self.api_key = api_key
+        self.api_adapter = api_adapter
+
+# Returns the list of all stops with their data (there's around 8000)
+    def get_all_stops(self):
+        try:
+            response = (requests.get(f'https://api.um.warszawa.pl/api/action/dbstore_get?id=ab75c33d-3a26-4342-b36a-6e5fef0a3ac3&apikey={self.api_key}').json())
+            return self.api_adapter.parse_api_stops(response)
+        except:
+            raise CouldNotConnect("Couldn't connect to the API")
+
+# Returns lines that are available at a given stop
+    def get_lines_for_stop(self, stop_id):
+        try:
+            response = requests.get(f'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=88cd555f-6f31-43ca-9de4-66c479ad5942&busstopId={int(stop_id/100)}&busstopNr={str(stop_id)[-2:]}&apikey={self.api_key}').json()
+            return response
+        except:
+            raise CouldNotConnect("Couldn't connect to the API")
+    
+# Returns a timetable for a specific stop and line.
+    def get_line_timetable(self, stop_id, line):
+        try:
+            response = requests.get(f'https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId={int(stop_id/100)}&busstopNr={str(stop_id)[-2:]}&line={line}&apikey={self.api_key}').json()
+            return response
+        except:
+            raise CouldNotConnect("Couldn't connect to the API")    
+
+# Returns the lines for a number of stops.
+    def get_lines_for_stops(self, stop_ids):
+        stops_lines = {}
+        for stop in stop_ids:
+            stop_buses = self.api_adapter.parse_api_lines(self.get_lines_for_stop(stop))
+            stops_lines[stop] = stop_buses
+        return stops_lines
+    
+# Returns the timetables for a list of stops.
+    def get_timetables_for_stops(self, stop_ids):
+        stops_timetables = {}
+        stop_lines = self.get_lines_for_stops(stop_ids)
+        for stop_id in stop_lines:
+            stops_timetables[stop_id] = {}
+            for line in stop_lines[stop_id]:
+                stops_timetables[stop_id][line] = self.api_adapter.parse_api_timetables(self.get_line_timetable(stop_id, line))
+        return stops_timetables
+
+#===================================================API ADAPTER==================================================
+# to understand the specific changes done to the data,
+# see "Opis api.md" for api response structure
+class ApiAdapter:
+    def __init__(self):
+        pass
+
+    def v(self, thing):
+        return thing['value']
+
+# takes in the api response, returns a list of tuples with the stop data
+    def parse_api_stops(self, api_stops_response):
+        if isinstance(api_stops_response['result'], str):
+            raise IncorrectApiCall('Api call failed')
+            return None
+        stops = []
+        for stop_response in api_stops_response['result']:
+            stop_response = stop_response['values']
+            stop_id = self.v(stop_response[0]) + self.v(stop_response[1])
+            stop_name = self.v(stop_response[2])
+            stop_geo_1 = self.v(stop_response[4])
+            stop_geo_2 = self.v(stop_response[5])
+            stop_destination = self.v(stop_response[6])
+            stop_start_date = self.v(stop_response[7])
+            stop_data = (int(stop_id), stop_name, float(stop_geo_1), float(stop_geo_2), stop_destination, stop_start_date)
+            stops.append(stop_data)
+        return stops
+
+# takes in the api response, returns a list of lines
+    def parse_api_lines(self, api_lines_response):
+        if isinstance(api_lines_response['result'], str):
+            raise IncorrectApiCall('Api call failed')
+            return None
+        lines = []
+        for line_dict in api_lines_response['result']:
+            line_info = line_dict['values']
+            for line in line_info:
+                lines.append(self.v(line))
+        return lines
+
+# takes in the api response, returns a dict with the data
+    def parse_api_timetables(self, api_timetables_response):
+        if isinstance(api_timetables_response['result'], str):
+            raise IncorrectApiCall('Api call failed')
+            return None
+        time_tables = []
+        for time_table in api_timetables_response['result']:
+            t_t_i = time_table['values']
+            time_table_entr = (self.v(t_t_i[2]), self.v(t_t_i[3]), self.v(t_t_i[4]), self.fix_time(self.v(t_t_i[5])))
+            time_tables.append(time_table_entr)
+        return time_tables
+
+# For some reason the API thinks it's ok to pass back a time value of "27:22:00"
+# I have absolutely zero idea why, but that's how it works so i need a function to fix it.
+    def fix_time(self, time):
+        # turning arbitrary number of hours into max 24 h
+        tms = time.split(':')
+        time_delta = timedelta(hours=int(tms[0]), minutes=int(tms[1]), seconds=int(tms[2]))
+        # adding the leading zero and returning
+        bad = datetime.strptime(str(timedelta(seconds=time_delta.seconds)), '%H:%M:%S')
+        return datetime.strftime(bad, '%H:%M:%S')
---
+++ backend-database/src/database_classes.py
@@ -0,0 +1,150 @@
+import sqlite3
+from datetime import datetime
+
+class DatabaseError(Exception):
+    pass
+
+# Functions that are deterministic (can be tested):
+# DatabaseAdapter:
+# - parse database stop ids
+# - turn timetables into query
+# - parse times
+#
+# DatabaseFacade:
+# None, all of them use the database, mocking a database would be pointless since
+# it would just be testing our mock methods, and not the interactions with an actual database
+
+# Status of tests: All written, all passing
+
+#===========================================DATABASE ADAPATER===========================================
+# Makes the data from the database facade more comfortable to work with
+class DatabaseAdapter:
+    def __init__(self) -> None:
+        pass
+
+# turns the result of a query into a list
+    def parse_database_stop_ids(self, stops):
+        n_stops = []
+        for stop in stops:
+            n_stops.append(stop[0])
+        return n_stops
+    
+# turns the dict used for easy access to data into a form that can be put into INSERT queries
+    def turn_timetables_into_query(self, timetables):
+        now = datetime.now()
+        result = []
+        for stop in timetables:
+            for line in timetables[stop]:
+                for time in timetables[stop][line]:
+                    print(time)
+                    result.append((stop, line, time[1], time[3], time[0], time[2], str(now)))
+        return result
+    
+# turn the result of a query into a table of times starting with now and going up (and back around if necessary)
+    def parse_times(self, times):
+        times = list(times)
+        now = datetime.strftime(datetime.now(), '%H:%M:%S')
+        now_in_times = 0
+        for id, time in enumerate(times):
+            if time[2] >= now:
+                now_in_times = id
+                break
+        n_times = times[now_in_times:]
+        n_times.extend(times[:now_in_times])
+        return n_times
+
+#======================================DATABASE FACADE===============================================
+# This Class acts as a simple interface with the internal database
+class DatabaseFacade:
+    def __init__(self, database_file, database_adapter):
+        self.data_adp = database_adapter
+        self.db_file = database_file
+
+# Function used to get results of a query, with proper cursor management.
+    def fetchall_query(self, query, values):
+        try:
+            con = sqlite3.connect(self.db_file)
+            cur = con.cursor()
+            result = cur.execute(query, values)
+            ret = result.fetchall()
+            cur.close()
+            con.close()
+            return ret
+        except:
+            raise DatabaseError('An Error with the database has occured')
+
+# Function used to execute a command and commit it to the database, with proper cursor management.
+    def execute_and_commit(self, query, values):
+        try:
+            con = sqlite3.connect(self.db_file)
+            cur = con.cursor()
+            cur.execute(query, values)
+            con.commit()
+            cur.close()
+            con.close()
+        except:
+            raise DatabaseError('An Error with the database has occured')
+
+# returns the contents of przystanki table
+    def get_stops(self):
+        return self.fetchall_query('SELECT * FROM przystanki', ())
+        
+# returns the contents of rozkład_jazdy table
+    def get_timetables(self):
+        return self.fetchall_query('SELECT * FROM rozkład_jazdy', ())
+
+# returns the data in przystanki table that is a relevant to a hash
+    def get_stops_hash(self):
+        return self.fetchall_query('SELECT id, wewnętrzne_id, nazwa, szer_geo, dlug_geo, odległość, kierunek, aktywny FROM przystanki', ())
+
+# returns the data in rozkład table that is relevant to a hash
+    def get_timetables_hash(self):
+        return self.fetchall_query('SELECT przystanki_id, linia, kierunek, czas_przyjazdu, brygada, trasa FROM rozkład_jazdy', ())
+
+# returns the ids of "active" stops (stops that have aktywny = '1')
+    def get_active_stops_ids(self):
+        res = self.fetchall_query('SELECT * FROM przystanki WHERE aktywny = "1"', ())
+        return self.data_adp.parse_database_stop_ids(res)
+    
+# returns the ids of "active" stops (stops that have aktywny = '1')
+    def get_active_stops(self):
+        res = self.fetchall_query('SELECT * FROM przystanki WHERE aktywny = "1"', ())
+        return res
+
+# returns n next buses/trams that will arrive at stop with given id
+    def get_n_next_times(self, stop_id, n):
+        res = self.data_adp.parse_times(self.fetchall_query('SELECT linia, kierunek, czas_przyjazdu, brygada, trasa, data_ostatniego_pobrania FROM rozkład_jazdy WHERE przystanki_id = ? ORDER BY czas_przyjazdu', (stop_id,)))
+        return res[:n]
+
+# Inserts new timetable data (and delete the old data)
+    def update_data(self, new_timetables):
+        try:
+            self.execute_and_commit('DELETE FROM rozkład_jazdy', ())
+            new_timetables_query = self.data_adp.turn_timetables_into_query(new_timetables)
+            con = sqlite3.connect(self.db_file)
+            cur = con.cursor()
+            tt_1 = new_timetables_query[0]
+            cur.execute(f"INSERT INTO rozkład_jazdy VALUES(1, ?, ?, ?, ?, ?, ?, strftime('%Y-%m-%d %H:%M:%f', ?))", tt_1)
+            cur.executemany(f"INSERT INTO rozkład_jazdy( przystanki_id, linia, kierunek, czas_przyjazdu, brygada, trasa, data_ostatniego_pobrania) VALUES(?, ?, ?, ?, ?, ?, strftime('%Y-%m-%d %H:%M:%f', ?))", new_timetables_query[1:])
+            con.commit()
+            cur.close()
+            con.close()
+        except:
+            raise DatabaseError('An Error with the database has occured')
+
+# Changes only the times in every entry to rozkład_jazdy, this method is used
+# when the data from the API has been checked and it's the same that it was before.
+    def update_time(self):
+        self.execute_and_commit("UPDATE rozkład_jazdy SET data_ostatniego_pobrania = strftime('%Y-%m-%d %H:%M:%f', ?)", (str(datetime.now()),))
+
+# Insert another row to the przystanki table
+    def add_stop(self, stop):
+        self.execute_and_commit(f'INSERT INTO przystanki VALUES(?)', (stop))
+
+# Remove a stop by id
+    def remove_stop(self, stop_id):
+        self.execute_and_commit(f'DELETE FROM przystanki WHERE id = ?', (stop_id))
+
+# Change the value of the 'Aktywny" status of a stop
+    def set_active_inactive(self, stop_id, new_status):
+        self.execute_and_commit(f'UPDATE przystanki SET aktywny = ? WHERE id = ?', (new_status, stop_id))
