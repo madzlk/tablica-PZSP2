59ef551bf3da3dd638626f8028d1646a8cee53d1
Franciszek <franekmalewski@gmail.com>
2024-04-21 05:08:05 +0200

    Added a main.py which runs the orchestrator such that the data is updated roughly every minute

---
+++ backend-database/src/main.py
@@ -0,0 +1,24 @@
+from orchestrator import Orchestrator
+import logging
+import time
+
+# Nothing to test here, just using other already made methods/functions.
+
+# This just wraps the python logging function into a dependency injection for the Orchestrator
+class Logger:
+    def __init__(self):
+        pass
+
+    def log(self, message):
+        logging.warning(message)
+
+# Calling the Orchestrator to update the data every minute, and printing any errors that appear.
+def main():
+    orch = Orchestrator(Logger())
+    while True:
+        orch.data_collection_cycle()
+        # This value was derived via testing; using this value the data if updated roughly once every minute.
+        time.sleep(36.5)
+
+if __name__ == '__main__':
+    main()
