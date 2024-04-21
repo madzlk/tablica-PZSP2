97a4e27f7119a0c88350f1b065d9eba597d34ff7
Franciszek <franekmalewski@gmail.com>
2024-04-21 08:35:44 +0200

    Created the file where Admin console will be, added imports and class definition

---
+++ backend-database/src/admin_console.py
@@ -0,0 +1,6 @@
+from api_classes import ApiAdapter, ApiFacade, DataObserver
+from database_classes import DatabaseAdapter, DatabaseFacade
+
+class AdminConsole():
+    def __init__(self) -> None:
+        pass
