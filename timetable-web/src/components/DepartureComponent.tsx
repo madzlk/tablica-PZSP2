20016ef6ae362919e981cc3c3a3ddbbbc0fd1523
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-04-24 17:03:02 +0200

    Components are now data-driven

--- timetable-web/src/components/DepartureComponent.tsx
+++ timetable-web/src/components/DepartureComponent.tsx
@@ -5,12 +5,35 @@ interface Props {
 }
 
 export const DepartureComponent = ({ departure }: Props) => {
+  let parseArrivalTime = (timeString: string) => {
+    let timeArray = timeString.split(":");
+  
+    let date = new Date();
+    date.setHours(parseInt(timeArray[0]));
+    date.setMinutes(parseInt(timeArray[1]));
+    date.setSeconds(parseInt(timeArray[2]));
+
+    return date;
+  };
+
+  let displayArrivalTime = (arrival: number, now: number) => {
+    const difference = Math.floor((arrival - now) / (1000 * 60));
+    if (difference === 0)
+    {
+      return "< 1";
+    }
+    return difference;
+  };
+
+  const currentTime = new Date().getTime();
+  const arrivalTime = parseArrivalTime(departure.czas_przyjazdu).getTime();
+
   return (
     <div className="flex flex-row justify-between">
       <div>
-        {departure.line} - {departure.direction}
+        {departure.linia} - {departure.kierunek}
       </div>
-      <div>{departure.arrivalTime.toTimeString().slice(0, 5)}</div>
+      <div>{displayArrivalTime(arrivalTime, currentTime)}</div>
     </div>
   );
 };
--- timetable-web/src/components/StopComponent.tsx
+++ timetable-web/src/components/StopComponent.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export const StopComponent = ({ stop }: Props) => {
     return (
-        <AdvancedMarker position={{lat: stop.lat, lng: stop.lng}}>
+        <AdvancedMarker position={{lat: stop.szer_geo, lng: stop.dlug_geo}}>
             <div style={{height: '30px', width: '60px'}}>
-                <p style={{color: 'blue'}}><b>{stop.stopName}</b></p>
+                <p style={{color: 'blue'}}><b>{stop.nazwa}</b></p>
             </div>
         </AdvancedMarker>
     );
