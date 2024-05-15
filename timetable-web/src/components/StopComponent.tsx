15ac5382ce5b93a39a4e1a1d641f60d944c128f8
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-15 17:33:50 +0200

    margin removed from classnames

--- timetable-web/src/components/StopComponent.tsx
+++ timetable-web/src/components/StopComponent.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const StopComponent = ({ stop, componentId }: Props) => {
     return (
         <AdvancedMarker position={{lat: stop.szer_geo, lng: stop.dlug_geo}}>
-            <div className="bg-[#7895CF] rounded-md px-1 text-xs lg:text-sm xl:text-base m-1 font-semibold text-white">
+            <div className="bg-[#7895CF] rounded-md px-1 text-xs lg:text-sm xl:text-base font-semibold text-white">
                 {componentId}
             </div>
         </AdvancedMarker>
