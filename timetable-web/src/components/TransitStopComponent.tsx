d2725f7742be8c2578366d04d5f293dc44cb804c
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-15 16:08:43 +0200

    Walking distance to stop added

--- timetable-web/src/components/TransitStopComponent.tsx
+++ timetable-web/src/components/TransitStopComponent.tsx
@@ -3,6 +3,7 @@ import { DepartureComponent } from "./DepartureComponent";
 import { useEffect, useState } from "react";
 import stops from "../services/stops";
 import { Departure } from "../types/Departure";
+import { BiWalk } from "react-icons/bi";
 
 interface Props {
   stop: TransitStop;
@@ -29,9 +30,14 @@ export const TransitStopComponent = ({ stop }: Props) => {
   return (
     <div className="flex flex-col bg-[#B49FAA] text-nowrap leading-tight">
       <div className="mb-1 bg-white p-1 px-2 text-xl font-semibold">
-        <h2>
-          {stop.nazwa} {" " + stop.id.toString().slice(-2)}
-        </h2>
+        <div className="flex flex-row justify-between">
+          <h2>
+            {stop.nazwa} {" " + stop.id.toString().slice(-2)}
+          </h2>
+          <div className="bg-[#7895CF] rounded-md px-1 text-xs lg:text-sm xl:text-base ml-1 font-semibold flex items-center">
+            <BiWalk/> {stop.odleglosc} min
+          </div>
+        </div>
       </div>
       <div className="flex flex-col justify-around gap-1 h-full p-1 text-white">
         {departures.map((departure) => (
